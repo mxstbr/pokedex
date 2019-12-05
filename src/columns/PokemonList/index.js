aab60a875b42306d6b58dc32522560392bb3d835
Max Stoiber <mxstbr@github.com>
2019-12-05 16:39:40 +0100

    Transitions

--- src/columns/PokemonList/index.js
+++ src/columns/PokemonList/index.js
@@ -5,8 +5,24 @@ import { Spinner } from "@nice-boys/components";
 import Sidebar from "../../components/Sidebar";
 import SidebarItem from "../../components/SidebarItem";
 import SidebarTitle from "../../components/SidebarTitle";
+import LoadingEllipsis from "../../components/LoadingEllipsis";
 import { fetchPokemons } from "../../api/pokeapi";
 import createResource from "../../resource";
+
+const PokemonSidebarItem = ({ pokemon, onClick }) => {
+  const [startTransition, isPending] = React.useTransition({
+    timeoutMs: 2000
+  });
+
+  return (
+    <Link onClick={() => startTransition(() => onClick())}>
+      <SidebarItem>
+        {pokemon.name}
+        {isPending && <LoadingEllipsis />}
+      </SidebarItem>
+    </Link>
+  );
+};
 
 const resource = createResource(fetchPokemons());
 
@@ -22,12 +38,11 @@ const PokemonList = props => {
         <Spinner />
       ) : (
         pokemons.map(pokemon => (
-          <Link
+          <PokemonSidebarItem
             key={pokemon.name}
+            pokemon={pokemon}
             onClick={() => props.setSelectedPokemon(pokemon.name)}
-          >
-            <SidebarItem>{pokemon.name}</SidebarItem>
-          </Link>
+          />
         ))
       )}
     </Sidebar>
