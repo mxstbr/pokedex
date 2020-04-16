7263228760d5080438bb94ab4670325d7f27d06b
Max Stoiber <contact@mxstbr.com>
2020-04-16 15:39:40 +0200

    PokemonDetails resources

--- src/columns/PokemonDetails/index.js
+++ src/columns/PokemonDetails/index.js
@@ -1,11 +1,12 @@
 // Renders the profile and games of a single pokemon
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Spinner } from "@nice-boys/components";
 import PokemonProfile from "../../components/PokemonProfile";
 import PokemonGamesSection from "../../components/PokemonGamesSection";
 import Column from "../../components/Column";
 import { fetchPokemonGames, fetchPokemonByName } from "../../api/pokeapi";
 import useAsync from "../../use-async";
+import createResource from "../../create-resource";
 
 const PokemonGames = props => {
   const [games, state] = useAsync(() => {
@@ -20,9 +21,12 @@ const PokemonGames = props => {
 };
 
 const Pokemon = props => {
-  const [pokemon, state] = useAsync(() => {
-    return fetchPokemonByName(props.name);
+  const [pokemonResource, setPokemonResource] = useState(null);
+  useEffect(() => {
+    setPokemonResource(createResource(fetchPokemonByName(props.name)));
   }, [props.name]);
+
+  const pokemon = pokemonResource ? pokemonResource.read() : null;
 
   return (
     <Column width={1} p={4}>
