abefdffe941420895c8e2c129addee7bb3eb1159
Max Stoiber <contact@mxstbr.com>
2020-04-16 11:06:20 +0200

    PokemonList => hooks

--- src/columns/PokemonList/index.js
+++ src/columns/PokemonList/index.js
@@ -1,5 +1,5 @@
 // Renders the sidebar with the list of pokemons in the pokedex
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "@primer/components";
 import { Spinner } from "@nice-boys/components";
 import Sidebar from "../../components/Sidebar";
@@ -7,40 +7,34 @@ import SidebarItem from "../../components/SidebarItem";
 import SidebarTitle from "../../components/SidebarTitle";
 import { fetchPokemons } from "../../api/pokeapi";
 
-class PokemonList extends React.Component {
-  state = {
-    pokemons: null
-  };
+const PokemonList = props => {
+  const [pokemons, setPokemons] = useState(null);
 
-  componentDidMount() {
+  useEffect(() => {
     fetchPokemons().then(pokemons => {
-      this.setState({
-        pokemons
-      });
+      setPokemons(pokemons);
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <Sidebar>
-        <Link onClick={() => this.props.setSelectedPokemon(null)}>
-          <SidebarTitle>Pokedex</SidebarTitle>
-        </Link>
-        {!this.state.pokemons ? (
-          <Spinner />
-        ) : (
-          this.state.pokemons.map(pokemon => (
-            <Link
-              key={pokemon.name}
-              onClick={() => this.props.setSelectedPokemon(pokemon.name)}
-            >
-              <SidebarItem>{pokemon.name}</SidebarItem>
-            </Link>
-          ))
-        )}
-      </Sidebar>
-    );
-  }
-}
+  return (
+    <Sidebar>
+      <Link onClick={() => props.setSelectedPokemon(null)}>
+        <SidebarTitle>Pokedex</SidebarTitle>
+      </Link>
+      {!pokemons ? (
+        <Spinner />
+      ) : (
+        pokemons.map(pokemon => (
+          <Link
+            key={pokemon.name}
+            onClick={() => props.setSelectedPokemon(pokemon.name)}
+          >
+            <SidebarItem>{pokemon.name}</SidebarItem>
+          </Link>
+        ))
+      )}
+    </Sidebar>
+  );
+};
 
 export default PokemonList;
