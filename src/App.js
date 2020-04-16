5c3f3787ad4f583739d3a655f0efd0657edab663
Max Stoiber <contact@mxstbr.com>
2020-04-16 10:48:00 +0200

    App.js => useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // The <App /> component is responsible for rendering the two main columns
-import React from "react";
+import React, { useEffect } from "react";
 import { BaseStyles } from "@primer/components";
 import { Flex } from "@primer/components";
 import PokemonList from "./columns/PokemonList";
@@ -7,6 +7,10 @@ import PokemonDetails from "./columns/PokemonDetails";
 
 const App = () => {
   const [selectedPokemon, setSelectedPokemon] = React.useState(null);
+
+  useEffect(() => {
+    document.title = `${selectedPokemon ? `${selectedPokemon} | ` : ""}Pokedex`;
+  });
 
   return (
     <BaseStyles>
@@ -17,34 +21,4 @@ const App = () => {
     </BaseStyles>
   );
 };
-
-// class App extends React.Component {
-//   state = {
-//     selectedPokemon: null
-//   };
-
-//   setSelectedPokemon = name => {
-//     this.setState({
-//       selectedPokemon: name
-//     });
-//   };
-
-//   componentDidMount() {
-//     this.updateDocumentTitle();
-//   }
-
-//   componentDidUpdate(_, prevState) {
-//     if (prevState.selectedPokemon !== this.state.selectedPokemon) {
-//       this.updateDocumentTitle();
-//     }
-//   }
-
-//   updateDocumentTitle() {
-//     const { selectedPokemon } = this.state;
-//     document.title = `${selectedPokemon ? `${selectedPokemon} | ` : ""}Pokedex`;
-//   }
-
-//   render() {}
-// }
-
 export default App;
